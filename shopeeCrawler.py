2b5f20e597759d7b38d599338e98383eb7fe7a1e
Jimting <handsome841206@gmail.com>
2020-04-30 18:40:39 +0800

    Update shopeeCrawler.py

--- shopeeCrawler.py
+++ shopeeCrawler.py
@@ -13,21 +13,21 @@ import pandas as pd
 
 def shopeeSearch (keyword) :
 
-    shopee_product_ad = crawler_shopee_product_ad(keyword)
-    shopee_product_info = crawler_shopee_product_info(keyword)
+	shopee_product_ad = crawler_shopee_product_ad(keyword)
+	shopee_product_info = crawler_shopee_product_info(keyword)
 
-    for i in range(len(shopee_product_ad)):
-        product_ad = shopee_product_ad.iloc[i]['name']
-        shopee_product_info.loc[shopee_product_info['name'] == product_ad, 'ad'] = True
-      
-    ad_num = len(shopee_product_ad)
-    print(keyword + '有 {} 個廣告'.format(ad_num))
-    shopee_product_info['sales_volume'] = shopee_product_info['sales_volume'].astype('int')
-    shopee_product_info = shopee_product_info.sort_values(by='sales_volume', ascending=False)
-    shopee_product_info['key'] = range(1, len(shopee_product_info) + 1) # 增加 index 欄位
-    shopee_product_info['ad_num'] = ad_num
+	for i in range(len(shopee_product_ad)):
+		product_ad = shopee_product_ad.iloc[i]['name']
+		shopee_product_info.loc[shopee_product_info['name'] == product_ad, 'ad'] = True
+	  
+	ad_num = len(shopee_product_ad)
+	print(keyword + '有 {} 個廣告'.format(ad_num))
+	shopee_product_info['sales_volume'] = shopee_product_info['sales_volume'].astype('int')
+	shopee_product_info = shopee_product_info.sort_values(by='sales_volume', ascending=False)
+	shopee_product_info['key'] = range(1, len(shopee_product_info) + 1) # 增加 index 欄位
+	shopee_product_info['ad_num'] = ad_num
 
-    return shopee_product_info.to_json(orient='records', force_ascii=False)
+	return shopee_product_info.to_json(orient='records', force_ascii=False)
 
 def fetch_page (keyword, page) :
 	options = Options()
@@ -58,84 +58,88 @@ def fetch_page (keyword, page) :
 	
 # 從html取出商品array
 def get_article_arr(page_html):
-    article_arr = []
-    host = 'https://shopee.tw'
-    ad_articles = page_html.select('[data-sqe="ad"]')
-    ad_articles_len = len(ad_articles)
+	article_arr = []
+	host = 'https://shopee.tw'
+	ad_articles = page_html.select('[data-sqe="ad"]')
+	ad_articles_len = len(ad_articles)
 
-    for article in ad_articles:
-        try:
-            article = article.parent.parent.parent.parent.parent.parent  # 回到 .shopee-search-item-result__item 那層
-            name = article.select('[data-sqe="name"]')[0].text
-            ad = True if len(article.select('[data-sqe="ad"]')) > 0 else False
-            article_arr.append({
-                'name': name,
-                'ad': ad
-            })
-        except Exception as e:
-            print(e)
-            print('---')
-            
-    return article_arr
+	for article in ad_articles:
+		try:
+			article = article.parent.parent.parent.parent.parent.parent	 # 回到 .shopee-search-item-result__item 那層
+			name = article.select('[data-sqe="name"]')[0].text
+			ad = True if len(article.select('[data-sqe="ad"]')) > 0 else False
+			article_arr.append({
+				'name': name,
+				'ad': ad
+			})
+		except Exception as e:
+			print(e)
+			print('---')
+			
+	return article_arr
 
 # 設置爬取的關鍵字，及從第幾頁開始爬
 # 回傳商品 df
 def crawler_shopee_product_ad(keyword, page = 0):
-    is_fetch_next_page = True # 判斷是否繼續爬下一頁
-    df_article_total_arr = pd.DataFrame([])
-    
-    while is_fetch_next_page:
-        page_html = fetch_page(keyword, page)
-        article_arr = get_article_arr(page_html)
-        df_article_arr = pd.DataFrame(article_arr)
-        df_article_total_arr = pd.concat([df_article_total_arr, df_article_arr], ignore_index=True)
-        if len(df_article_arr) < 10:
-            is_fetch_next_page = False
-        page += 1
+	is_fetch_next_page = True # 判斷是否繼續爬下一頁
+	df_article_total_arr = pd.DataFrame([])
+	
+	while is_fetch_next_page:
+		page_html = fetch_page(keyword, page)
+		article_arr = get_article_arr(page_html)
+		df_article_arr = pd.DataFrame(article_arr)
+		df_article_total_arr = pd.concat([df_article_total_arr, df_article_arr], ignore_index=True)
+		if len(df_article_arr) < 10:
+			is_fetch_next_page = False
+		page += 1
 
-    return df_article_total_arr
+	return df_article_total_arr
 
 # 設置爬取的關鍵字，及從第幾頁開始爬
 # 回傳商品 df
 def crawler_shopee_product_info(keyword, page = 1):
-    import pandas as pd
-    import re
-    
-    article_arr = []
-    host = 'https://shopee.tw'
-    
-    for i in range(page):
+	import pandas as pd
+	import re
+	
+	article_arr = []
+	host = 'https://shopee.tw'
+	
+	for i in range(page):
 		# url = 'https://shopee.tw/search?keyword={0}&page={1}&sortBy=sales'.format(keyword, i)
-        # print(url)
-        # headers = {
-        #    'user-agent': 'Googlebot'
-        # }
-        # resp = requests.get(url, headers=headers) 
-        soup = fetch_page(keyword, i)
+		# print(url)
+		# headers = {
+		#	 'user-agent': 'Googlebot'
+		# }
+		# resp = requests.get(url, headers=headers) 
+		soup = fetch_page(keyword, i)
 
-        articles = soup.select('.shopee-search-item-result__item')
-        for article in articles:
-            name = article.select('[data-sqe="name"]')[0].text
-            link = host + article.select('a')[0]['href']
-            img = article.select('a > div > div > img')[0]['src']
-            sales_volume = '0' if article.select('[data-sqe="rating"]')[0].next_sibling.text == '' else article.select('[data-sqe="rating"]')[0].next_sibling.text
-            sales_volume = re.findall(r'\d+', sales_volume)[0]
-            review = len(article.select('.shopee-rating-stars__stars .shopee-rating-stars__star-wrapper'))
-            price = article.select('[data-sqe="name"]')[0].next_sibling.text.replace('$', '').replace(',', '')
-            if len(price.split(' - ')) != 1:
-                price = (int(price.split(' - ')[0]) + int(price.split(' - ')[1])) / 2
-            monthly_revenue = float(sales_volume) * float(price)
+		articles = soup.select('.shopee-search-item-result__item')
+		for article in articles:
+			try:
+				name = article.select('[data-sqe="name"]')[0].text
+				link = host + article.select('a')[0]['href']
+				img = article.select('a > div > div > img')[0]['src']
+				sales_volume = '0' if article.select('[data-sqe="rating"]')[0].next_sibling.text == '' else article.select('[data-sqe="rating"]')[0].next_sibling.text
+				sales_volume = re.findall(r'\d+', sales_volume)[0]
+				review = len(article.select('.shopee-rating-stars__stars .shopee-rating-stars__star-wrapper'))
+				price = article.select('[data-sqe="name"]')[0].next_sibling.text.replace('$', '').replace(',', '')
+				if len(price.split(' - ')) != 1:
+					price = (int(price.split(' - ')[0]) + int(price.split(' - ')[1])) / 2
+				monthly_revenue = float(sales_volume) * float(price)
 
-            article_arr.append({
-                'name': name,
-                'link': link,
-                'img': img,
-                'sales_volume': sales_volume,  # 月銷售量
-                'price': price,  # 單價
-                'monthly_revenue': monthly_revenue,  # 月收加總
-                'review': review,  # 評價
-                'ad': False
-            })
+				article_arr.append({
+					'name': name,
+					'link': link,
+					'img': img,
+					'sales_volume': sales_volume,  # 月銷售量
+					'price': price,	 # 單價
+					'monthly_revenue': monthly_revenue,	 # 月收加總
+					'review': review,  # 評價
+					'ad': False
+				})
+			except Exception as e:
+				print(e)
+				print('---')
 
-    df = pd.DataFrame(article_arr, columns=['name', 'link', 'img', 'sales_volume', 'price', 'monthly_revenue', 'review', 'ad'])  # 使用 columns 調整排列順序
-    return df
+	df = pd.DataFrame(article_arr, columns=['name', 'link', 'img', 'sales_volume', 'price', 'monthly_revenue', 'review', 'ad'])	 # 使用 columns 調整排列順序
+	return df
